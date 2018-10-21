23d4e925aa5718e77c0ca795a788dbb31586d03f
Sean Mullan <seanmullan97@outlook.com>
2018-10-21 02:22:07 +0100

    Add preprocessor parsing and add sklearn to directory

--- src/lambda-preprocessing/processor.py
+++ src/lambda-preprocessing/processor.py
@@ -3,25 +3,47 @@ import io
 import boto3
 import json
 import csv
-from textblob import TextBlob
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.feature_extraction.text import TfidfTransformer
 
-ENDPOINT_NAME = " / TODO: get endpoint name "
+# grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
+    
+    # Parse JSON data
     print("Received event: " + json.dumps(event, indent=2))
     data = json.loads(json.dumps(event))
-
-def invokeEndpoint(data):
-    return 1
-
-def parseJson(data):
-    return 1
-  
-def getObjectivity(text):
-    return 1
-
-def getSubjectivity(text):
-    return 1
-  
-def getSize(text):
-    return 1
+    url = data['url']
+    title = data['title']
+    text = data['text']
+    total = title + text
+    
+    # Create dict of training vocab
+    vocab = json.loads(getVocab())
+    
+    # TODO: Transform data using vectorizer
+    # transformer = TfidfTransformer()
+    # loaded_vec = CountVectorizer(decode_error="replace", vocabulary=vocab)
+    # tfidf = transformer.fit_transform(loaded_vec.fit_transform(total))
+    
+    # TODO: Convert transformed data into csv
+    
+    # Invoke model on transformed data
+    # response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+    #                                   ContentType='text/csv',
+    #                                   Body=payload)
+                                       
+    # TODO: Return result from model
+    print(response)
+    result = json.loads(response['Body'].read().decode())
+    print(result)
+    pred = int(result['predictions'][0]['predicted_label'])
+    return pred
+    
+def getVocab():
+    client = boto3.client("s3")
+    result = client.get_object(Bucket="fakenewsdata", Key="sagemaker/DEMO-xgboost-regression/vocab/vocab.json")
+    return result["Body"].read().decode()
