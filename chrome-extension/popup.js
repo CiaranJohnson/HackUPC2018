87c882185dcd6ae2c68606eab02c3ce6e22c4118
Gabriel Mawhinney <gabrielmawhinney@gmail.com>
2018-10-21 08:01:19 +0200

    Tidied up code

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -10,36 +10,27 @@ var apigClient = apigClientFactory.newClient({
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     userUrl = tabs[0].url;
 
-    var body = {
-        url: userUrl
-    };
-
+    var body = { url: userUrl };
     var scraped = {"url": "", "title": "", "text": ""};
 
     // Webscrape url and return JSON of scraped data
     apigClient.webscraperPost({}, body)
     .then(function(result){
         console.log(userUrl + " scraped successfully");
-        console.log(result.data["title"]);
-        console.log(result.data["text"]);
         scraped.title = result.data["title"];
         scraped.text = result.data["text"];
 
         apigClient.modelPost({}, scraped)
          .then(function(result){
            console.log("Fake news score predicted successfully");
-           console.log(scraped.title);
-           console.log(result.data);
-           //calculate.style.display = "none";
-           //score.style.display = "block";
            score.innerHTML = "Score: " + result.data;
            
+           // Shows an appropriate picture of Pinocchio depending on prediction article receives
            if(parseFloat(result.data) < 5) {
             score.insertAdjacentHTML("beforebegin", "<img class='pinocchio' src='/images/pinocchio-fake.png'>");
            } else {
             score.insertAdjacentHTML("beforebegin", "<img class='pinocchio' src='/images/pinocchio-real.png'>")
            };
-
          }).catch( function(result){
            console.log("Failed to predict fake news score");
            console.log(result);
@@ -48,15 +39,4 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         console.log("Failed to scrape " + userUrl);
     });
 
-    console.log(typeof(scraped));
-    //console.log(scraped["title"]);
-    //console.log(scraped.data["title"]);
-
-    // Pass JSON object to be preprocessed and calculate fake news score prediction
-    
-
 });
-
-// Shows an appropriate picture of Pinocchio depending on prediction article receives
-console.log(prediction.score);
-console.log(typeof(prediction.score));
--- src/lambda-preprocessing/processor.py
+++ src/lambda-preprocessing/processor.py
@@ -4,19 +4,17 @@ import boto3
 import json
 import csv
 import numpy as np
-
-# Lets try some stuff out
 import sklearn
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
-# grab environment variables
+# Get environment variables
 ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
 runtime= boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
-    
     # Parse JSON data
     print("Received event: " + json.dumps(event, indent=2))
     data = json.loads(json.dumps(event))
@@ -29,16 +27,14 @@ def lambda_handler(event, context):
     # Create dict of training vocab
     vocab = json.loads(getVocab())
     
-    # TODO: Transform data using vectorizer
+    # Transform data using vectorizer
     transformer = TfidfTransformer()
     loaded_vec = CountVectorizer(decode_error="replace", vocabulary=vocab)
     print("Type of loaded_vec {}".format(type(loaded_vec)))
-    #print("Type of loaded_vec.fit_transform(total): {}".format(type(loaded_vec.fit_transform(total))))
     tfidf = transformer.fit_transform(loaded_vec.transform(total))
     
-    # TODO: Convert transformed data into csv
+    # Convert transformed data into csv
     tfidf = tfidf.toarray()
-    print("tfidf shape: {}".format(tfidf.shape))
     payload = np2csv(tfidf)
 
     # Invoke model on transformed data
@@ -46,16 +42,9 @@ def lambda_handler(event, context):
                                        ContentType='text/csv',
                                        Body=payload)
                                        
-    # TODO: Return result from model
-    print(response)
+    # Return result from model
     result = json.loads(response['Body'].read().decode())
-    #x = { 'result' : str(result)}
-    #y = json.dumps(x)
-    print("Result:")
-    print(result)
-    print(type(result))
-    #pred = int(result['predictions'][0]['predicted_label'])
-    return ((1-result)*10)
+    return round(((1-result)*10), 4)
     
 def getVocab():
     client = boto3.client("s3")
